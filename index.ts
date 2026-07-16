0a74bd86667e8396ff052b3a659db7f0eb63401d
Obed Danso <dansoeffah1@gmail.com>
2026-07-16 19:17:15 -0400

    refactor: update package.json metadata and export statements in index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello via Bun!");
+export * from "./lib/Mediator";
+export * from "./lib/Message";
+export * from "./lib/CommandHandlerBase";
+export * from "./lib/QueryHandlerBase";
+export * from "./lib/LoggerBase";
